4795ad827e4db42d19d6178622680a8243a08849
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-06-25 21:56:50 -0300

    Finalizando o aquecimento

--- JavaScript/aprendendo.js
+++ JavaScript/aprendendo.js
@@ -1,2 +1,15 @@
 const read = require('readline-sync');
-let nome = 
+//Pedindo o nome do usuário
+let nome = read.question("Qual seu nome?");
+console.log(nome);
+
+//Recebendo os valores
+let n1 = read.question("Digite o primeiro número: ");
+let n2 = read.question("Digite o segundo número: ");
+
+//É necessário converter para float/int
+//pois quando o JS recebe um valor ele tranforma automáticamente em string
+
+let resul = parseFloat(n1) + parseFloat(n2);
+//Mostrando o nome e o resultado na tela
+console.log(nome + "o resultado deu: " + resul)
