1437da18bab6dc585e1e66093236fd420d5bef1f
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-06-30 20:02:17 -0300

    Finished the first exercise do and While

--- JavaScript/doWhile/notaAlunoValidacao.js
+++ JavaScript/doWhile/notaAlunoValidacao.js
@@ -1,4 +1,18 @@
 const read = require('readline-sync');
 
-let alunos = 0;
+let alunos = 1;
 
+do {
+    console.log("Notas dos ALUNOS");
+    do {
+        var nota1 = read.question(`Digite a primeira nota do aluno, ${alunos}: `);
+        console.clear
+    } while (nota1 > 0  && nota1 > 10);
+    do {
+        var nota2 = read.question(`Digite a segunda nota do aluno, ${alunos}: `);
+    } while (nota2 > 0 && nota2 > 10);
+
+    let media = (parseFloat(nota1) + parseFloat(nota2))/2;
+
+    console.log(`A média do aluno ${alunos}, é igual: ${media}`)
+} while (alunos < 11);
