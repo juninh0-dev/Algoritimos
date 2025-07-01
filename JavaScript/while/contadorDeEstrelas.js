90988012aea4272ce46d53e28f45998ded789dfe
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-06-30 22:50:32 -0300

    Finished while exercise

--- JavaScript/while/contadorDeEstrelas.js
+++ JavaScript/while/contadorDeEstrelas.js
@@ -1,6 +1,21 @@
 const read = require('readline-sync');
 
-let parar = 's';
-while{
+let parar = '';
+var estrelas = 0;
 
-}(parar != 'n' || parar != 'N');
+while (parar !== 'n') {
+
+
+    
+    parar = read.question("Você viu uma estrela?(S/N): ").toLowerCase();
+
+    if(parar == 's'){
+        estrelas++;
+    }else{
+        console.clear();
+        console.log("Digite S ou N.");
+    }
+
+};
+
+console.log("Você avistou um total de: " + estrelas + " estrelas.");
