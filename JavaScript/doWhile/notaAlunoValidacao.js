46240a3ffaa0c9da6af953432a424128356e872e
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-06-30 22:05:56 -0300

    Fixing issue with checking grade greater than 10 and less than 0

--- JavaScript/doWhile/notaAlunoValidacao.js
+++ JavaScript/doWhile/notaAlunoValidacao.js
@@ -6,10 +6,23 @@ do {
     console.log("Notas dos ALUNOS");
     do {
         var nota1 = read.question(`Digite a primeira nota do aluno, ${alunos}: `);
-        console.clear
+        console.clear()
+        if(nota1 < 0){
+            console.log("Digite uma nota igual ou maior que 0.");
+        } else if(nota1 > 10){
+            console.log("Digite uma nota menor ou igual a 10.");
+        }
+
     } while (nota1 > 0  && nota1 > 10);
     do {
         var nota2 = read.question(`Digite a segunda nota do aluno, ${alunos}: `);
+        console.clear()
+        if(nota2 < 0){
+            console.log("Digite uma nota igual ou maior que 0.");
+        } else if(nota2 > 10){
+            console.log("Digite uma nota menor ou igual a 10.");
+        }
+        
     } while (nota2 > 0 && nota2 > 10);
 
     let media = (parseFloat(nota1) + parseFloat(nota2))/2;
