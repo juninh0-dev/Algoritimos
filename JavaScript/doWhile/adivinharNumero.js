b0f2785f9c6aef4ba0b501260ef63c7f4e5e3199
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-06-30 22:27:08 -0300

    Almost finished the exercise, now It's challenge random NUMBER

--- JavaScript/doWhile/adivinharNumero.js
+++ JavaScript/doWhile/adivinharNumero.js
@@ -1,13 +1,21 @@
-let randomNumber = 28;
 const read = require('readline-sync');
 
-do{
-    let number = read.question("Digite um número: ");
+let randomNumber = 28;
+
+let contador = 0;
+
+do{ 
+    var number = read.question("Digite um número: ");
 
     if(number > randomNumber){
+        console.clear()
         console.log("É um número menor.");
     }else if(number < randomNumber){
-        console.log("")
+        console.clear()
+        console.log("É um número maior.");
     }
 
-}while(number == randomNumber);
+    contador++;
+}while(number != randomNumber);
+
+console.log("NICE! Você acertou. O número correto é " + randomNumber + ". Tentativas: " + contador);
