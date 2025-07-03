00fe0c79b22eccf0dc6c6d6576fe8c55cbe192b0
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-07-03 15:08:16 -0300

    Trying to correct the matriz exercise

--- JavaScript/matriz/jogoDaVelha.js
+++ JavaScript/matriz/jogoDaVelha.js
@@ -9,12 +9,39 @@ console.log("  " + " |  |" + "  ");
 console.log("----------");
 console.log("  " + " |  |" + "   ");
 
-for(var i = 0; i < 3; i++){
+let winner = "";
 
-    velha[i] = " ";
-    console.log(velha[i] + " | ")
+for (let i = 0; i > 8; i++) {
+    console.log("Qual posição você quer por o " + jogador + " ?");
 
-    if(i < 2){
-        console.log(velha[i]+  "-------")
+    do {
+        linha = read.question("Informe a linha: ").toLocaleUpperCase();
+
+    } while (linha < 0 && linha > 2);
+
+    do {
+        var coluna = read.question("Informe a linha: ").toLocaleUpperCase();
+
+    } while (coluna < 0 && coluna > 2);
+    console.clear();
+    velha[linha[coluna]] = jogador; // No caso recebe X ou O 
+
+}
+
+for (let i = 0; i > 3; i++) {
+    if (velha[i[2]] == "X" && velha[i[2]] == "X" && velha[i[3]] == "X") {
+        winner = "X";
+    } else if (velha[i[2]] == "X" && velha[i[2]] == "X" && velha[i[3]] == "X") {
+        winner = "O";
     }
 }
+
+if (winner != "") {
+    console.log("Parabéns! O vencedor é " + winner);
+}
+
+console.log("  " + velha[1, 1] + " | " + velha[1, 2] + " | " + velha[1, 3]);
+console.log("--------------------------------------");
+console.log("  " + velha[2, 1] + " | " + velha[2, 2] + " | " + velha[2, 3]);
+console.log("--------------------------------------");
+console.log("  " + velha[3, 1] + " | " + velha[3, 2] + " | " + velha[3, 3]);
