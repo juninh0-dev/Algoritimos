ad295033ec1469cc9318bec1cacb3fc6a9d2480b
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-07-02 22:16:52 -0300

    Finished second Array exercise

--- JavaScript/array/notasDosAlunosArray.js
+++ JavaScript/array/notasDosAlunosArray.js
@@ -2,16 +2,18 @@ const read = require('readline-sync');
 
 notas = [];
 
-for(let i = 0; i < 10; i++){
-    notas[i] = read.question(`Digite a nota do aluno número ${i} `);
+for (let i = 0; i < 10; i++) {
+    notas[i] = parseFloat(read.question(`Digite a nota do aluno número ${i + 1}: `));
+
+    var media = 0;
+    media = media + notas[i]
 }
+console.clear();
+for (let i = 0; i < 10; i++) {
 
-notas = parseFloat(notas);
-
-for(let i = 0; i < 10; i++){
-    if(notas[i] > 6){
-        console.log(`O aluno ${i} ficou ACIMA da média ${notas[i]}`);
-    } else{
-        console.log(`O aluno ${i} ficou ABAIXO da média ${notas[i]}`);
+    if (notas[i] >= media) {
+        console.log(`O aluno ${i + 1} ficou ACIMA da média. Nota:  ${notas[i]}`);
+    } else {
+        console.log(`O aluno ${i + 1} ficou ABAIXO da média. Nota: ${notas[i]}`);
     }
 }
