299c95e6fea39a2671d3aa9e1243e5f6b16bff16
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-07-03 10:43:15 -0300

    Starting the matriz exercise

---
+++ JavaScript/matriz/jogoDaVelha.js
@@ -0,0 +1,14 @@
+const read = require('readline-sync');
+
+let velha = [[]];
+
+console.log(" X " + "| X |" + " X ");
+console.log("  _ " + "  _ " + "  _ ");
+console.log(" X " + "| X |" + " X ");
+console.log("  _ " + "  _ " + "  _ ");
+console.log(" X " + "| X |" + " X ");
+
+for(let i = 0; i < 3; i++){
+    for(let j = 0; j < 3; j++){
+    }
+}
