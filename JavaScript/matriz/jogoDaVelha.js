e6dcfe2307bf5c9fd4da61a378f968ba2720d298
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-07-04 21:58:50 -0300

    Fixing the Errors

--- JavaScript/matriz/jogoDaVelha.js
+++ JavaScript/matriz/jogoDaVelha.js
@@ -14,47 +14,46 @@ console.log("  " + " |  |" + "  ");
 console.log("----------");
 console.log("  " + " |  |" + "   ");
 
-let winner = "";    
+let winner = "";
 
-for (let i = 0; i < 8; i++) {
+do {
     console.log("Qual posição você quer por o " + jogador + " ?");
     var linha, coluna;
-        do {
-            linha = parseInt(read.question("Informe a linha: "));
+    do {
+        linha = parseInt(read.question("Informe a linha: "));
 
-        } while (linha < 0 || linha > 2);
+    } while (linha < 0 || linha > 2);
 
-        do {
-            coluna = parseInt(read.question("Informe a coluna: "));
+    do {
+        coluna = parseInt(read.question("Informe a coluna: "));
 
-        } while (coluna < 0 || coluna > 2);
-        
+    } while (coluna < 0 || coluna > 2);
+    console.clear();
+    velha[linha][coluna] = jogador; // No caso recebe X ou O
 
-        if(jogador == "X"){
-            jogador = "O";
-        }else if(jogador == "O"){
-            jogador = "X"
-        }
-
-        console.clear();
-        velha[linha][coluna] = jogador; // No caso recebe X ou O
-
-        console.log("  " + velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2]);
-        console.log("--------------------------------------");
-        console.log("  " + velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2]);
-        console.log("--------------------------------------");
-        console.log("  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[1][2]);
-
-        
-}
-
-for (let i = 0; i < 2; i++) {
-    if (velha[i][0] == "X" && velha[i][1] == "X" && velha[i][2] == "X") {
-        winner = "X";
-    } else if (velha[i][0] == "O" && velha[i][1] == "O" && velha[i][2] == "O") {
-        winner = "O";
+    if (jogador == "X") {
+        jogador = "O";
+    } else if (jogador == "O") {
+        jogador = "X"
     }
-}
+
+    console.log("  " + velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2]);
+    console.log("--------------------------------------");
+    console.log("  " + velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2]);
+    console.log("--------------------------------------");
+    console.log("  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2]);
+
+    for (let i = 0; i < 2; i++) {
+        if (velha[i][0] == "X" && velha[i][1] == "X" && velha[i][2] == "X") {
+            winner = "X";
+        } else if (velha[i][0] == "O" && velha[i][1] == "O" && velha[i][2] == "O") {
+            winner = "O";
+        }
+    }
+
+} while (winner != "X" || winner != "O");
+
+
 
 if (winner != "") {
     console.log("Parabéns! O vencedor é " + winner);
@@ -64,4 +63,4 @@ console.log("  " + velha[0][0] + " | " + velha[0][1] + " | " + velha[0][2]);
 console.log("--------------------------------------");
 console.log("  " + velha[1][0] + " | " + velha[1][1] + " | " + velha[1][2]);
 console.log("--------------------------------------");
-console.log("  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[1][2]);
+console.log("  " + velha[2][0] + " | " + velha[2][1] + " | " + velha[2][2]);
