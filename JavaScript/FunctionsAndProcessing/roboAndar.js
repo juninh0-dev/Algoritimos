a2eeb198bcf5e827b0febcf3202f39334d5a9d47
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-07-08 10:23:07 -0300

    Finishing the AndarRobo exercise

--- JavaScript/FunctionsAndProcessing/roboAndar.js
+++ JavaScript/FunctionsAndProcessing/roboAndar.js
@@ -1,2 +1,20 @@
 const read = require('readline-sync');
 
+let passos = 0;
+
+function Andou(passo){
+    let mensg = 'O robô andou' + passo
+
+    passos++;
+}
+
+
+do{
+    let resp = read.question("O robô andou?(S/N)");
+    if(resp == 'S' || 's'){
+        
+    }
+
+}while(resp != 'n' || resp !='N')
+
+console.log("Acabou a bateria, o robô parou de andar.");
