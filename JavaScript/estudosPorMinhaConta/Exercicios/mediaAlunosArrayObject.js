815562f7872ca160d1217a6f6a8d3f18170353ef
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-07-12 16:09:59 -0300

    Finalizando finalmente o exercicio 3 de array e object

--- JavaScript/estudosPorMinhaConta/Exercicios/mediaAlunosArrayObject.js
+++ JavaScript/estudosPorMinhaConta/Exercicios/mediaAlunosArrayObject.js
@@ -1,10 +1,40 @@
 //Exercício 3 – Média dos alunos
 
-let aluno = [
-    {nome: "Diego", nota: 9.5},
-    {nome: "Julio", nota: 5},
-    {nome: "Giovana", nota: 10}
-    {nome: "João", nota: 3}
-    {nome: "Bianca", nota: 7}
-];
+/*
+Crie um array de objetos alunos, cada um com:
 
+nome
+
+nota
+
+Depois, calcule a média e mostre os nomes dos alunos com nota maior ou igual a 7.
+
+*/
+
+let alunos = [
+    { nome: "Diego", nota: 9 },
+    { nome: "Julio", nota: 5 },
+    { nome: "Giovana", nota: 10 },
+    { nome: "João", nota: 3 },
+    { nome: "Bianca", nota: 10 }
+];
+let media = 0;
+
+for (let i = 0; i < alunos.length; i++) {
+    media += alunos[i].nota;
+
+    if (i == 4) {
+        media = media / (i + 1);
+
+        console.log("Média: " + media);
+
+        for (let i = 0; i < alunos.length; i++) {
+            if (alunos[i].nota >= media) {
+                console.log("O aluno, " + alunos[i].nome + " foi APROVADO! Nota: " + alunos[i].nota);
+            } else {
+                console.log("O aluno, " + alunos[i].nome + " foi REPROVADO! Nota: " + alunos[i].nota);
+            }
+        }
+    }
+
+}
