abd71fc651bec30aa86814b44f0dff05e04cab37
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-07-12 14:43:42 -0300

    Iniciando exercicio 3 sobre array e objeto

---
+++ JavaScript/estudosPorMinhaConta/Exercicios/mediaAlunosArrayObject.js
@@ -0,0 +1,10 @@
+//Exercício 3 – Média dos alunos
+
+let aluno = [
+    {nome: "Diego", nota: 9.5},
+    {nome: "Julio", nota: 5},
+    {nome: "Giovana", nota: 10}
+    {nome: "João", nota: 3}
+    {nome: "Bianca", nota: 7}
+];
+
